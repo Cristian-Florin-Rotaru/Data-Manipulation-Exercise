900efa074760d56e5a28ee5ea2ac665002e66a5d
Cristian-Florin Rotaru <crysss.floryn@gmail.com>
2019-10-20 07:29:57 +0100

    Changed INSERT method
    Used BULK INSERT method instead of inserting each row separately, reducing the total time from ~35 minutes to a few seconds.

--- JBA Task/Program.cs
+++ JBA Task/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 using System.IO;
 using System.Windows.Forms;
@@ -14,30 +14,32 @@ namespace JBA_Task
         static string textFile = "";
         static string dbConn = @"Server =.\SQLEXPRESS; Database = jbaDB; Trusted_Connection = True;";
         static string serverConn = @"Server =.\SQLEXPRESS; Trusted_Connection = True;";
-        static string table = "CREATE TABLE PRECIPITATION (ID int IDENTITY(1,1) PRIMARY KEY, Xref int, Yref int, Date DATE, Value int)";
-
-        public static int startYear = 0;
-        public static int endYear = 0;
-        public static int xRef = 0;
-        public static int yRef = 0;
-        public static int yearIndex = 0;
-        public static int monthIndex = 0;
+        static string createTableSQL = "CREATE TABLE PRECIPITATION (ID int IDENTITY(1,1) PRIMARY KEY, Xref int, Yref int, Date DATE, Value int)";
+        static string tableName = "PRECIPITATION";
+        static int startYear = 0;
+        static int endYear = 0;
+        static int xRef = 0;
+        static int yRef = 0;
+        static int yearIndex = 0;
+        static int monthIndex = 0;
+        static DataTable dataTable;
+        static long dtSize = 0;
+        
 
         [STAThread]
         static void Main(string[] args)
         {
 
             JBATask jba = new JBATask();
-            jba.createDatabase();
+            jba.CreateDatabase();
 
             SqlConnection conn = new SqlConnection(dbConn);
             try
             {
                 conn.Open();
-                jba.createTable(conn);
-                jba.setFilePath();
-                jba.processFile(conn);
-
+                jba.CreateTable(conn);
+                jba.SetFilePath();
+                jba.ProcessFile(conn);
 
             }
             catch (System.Exception ex)
@@ -52,25 +54,24 @@ namespace JBA_Task
                 }
             }
 
-            
-
             Console.ReadKey();
         }
 
 
-        public void processFile(SqlConnection conn)
+        public void ProcessFile(SqlConnection conn)
         {
             if (File.Exists(textFile))
             {
                 try
                 {
                     StreamReader file = new StreamReader(textFile);
-
+                    SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(conn);
+                    sqlBulkCopy.DestinationTableName = tableName;
+                    InitiateDataTable();
                     string ln;
                     bool searchYears = true;
-                    int currentLine = 0;
-
-                    Console.WriteLine("Started exporting data to DATABASE, Please wait");
+                    long currentLine = 0;
+                    Console.WriteLine("Started exporting data to DATABASE AT:" + DateTime.Now);
                     while ((ln = file.ReadLine()) != null)
                     {
                         currentLine++;
@@ -78,30 +79,36 @@ namespace JBA_Task
                         {
                             throw new System.ArgumentException("Period of time not found");
                         }
-                   
-                        if (!searchYears && !ln.Contains("Grid-ref=") && yearIndex <= endYear)
-                        {
-                            sendToDB(ln, conn);
-                            yearIndex++;
 
+                        if (dtSize > 50000)
+                        {
+                            sqlBulkCopy.WriteToServer(dataTable);
+                            InitiateDataTable();
+                            dtSize = 0;
+                        }
+
+                            if (!searchYears && !ln.Contains("Grid-ref=") && yearIndex <= endYear)
+                        {
+                            FillDataTable(ln);
+                            yearIndex++;
                         }
 
                         if (searchYears && ln.Contains("Years="))
                         {
                             searchYears = false;
-                            setYears(ln);
+                            SetYears(ln);
                             if (startYear > DateTime.Now.Year || startYear < 1900 || endYear > DateTime.Now.Year || endYear < 1900 || startYear > endYear )
                                 throw new System.ArgumentException("Provided wrong information about the period of time");
-
                         }
 
                         if (!searchYears && ln.Contains("Grid-ref="))
                         {
-                            setGridRef(ln);
+                            SetGridRef(ln);
                         }
 
                     }
                     file.Close();
+                    sqlBulkCopy.WriteToServer(dataTable);
                     Console.WriteLine("Finished exporting data to DATABASE");
                 }
                 catch (Exception e)
@@ -110,12 +117,33 @@ namespace JBA_Task
                 }
             }
         }
+        public void InitiateDataTable()
+        {
+            dataTable = new DataTable();
+            dataTable.Columns.Add("ID");
+            dataTable.Columns.Add("Xref");
+            dataTable.Columns.Add("yRef");
+            dataTable.Columns.Add("Date");
+            dataTable.Columns.Add("Value");
+
+        }
+        public void FillDataTable(string ln)
+        {
+            int[] values = DataLineToArray(ln);
+            string date;
+            for (int i = 1; i <= values.Length; i++)
+            {
+                date = yearIndex + "-" + i + "-1";
+                dataTable.Rows.Add(new Object[] { "", xRef, yRef, date, values[i - 1] });
+                dtSize++;
+            }
+        }
 
         /*
          * Opens a file dialog to pick up the file wanted to process
          **/
 
-        public void setFilePath()
+        public void SetFilePath()
         {
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.ShowDialog();
@@ -124,7 +152,7 @@ namespace JBA_Task
         /*
          * Sets the years the data from the file was recorded 
          **/
-        public void setYears(string ln)
+        public void SetYears(string ln)
         {
             string years = ln.Split(new string[] { "Years=" }, StringSplitOptions.None)[1].Split(']')[0];
             int.TryParse(years.Split('-')[0], out startYear);
@@ -134,17 +162,18 @@ namespace JBA_Task
         /*
          * Sets the GridRef given before each set of data
          **/
-        public void setGridRef(string ln)
+        public void SetGridRef(string ln)
         {
             ln = ln.Split(new string[] { "Grid-ref=" }, StringSplitOptions.None)[1].Replace(" ", "");
             int.TryParse(ln.Split(',')[0], out xRef);
             int.TryParse(ln.Split(',')[1], out yRef);
             yearIndex = startYear;
         }
+
         /* Has a line of data(12 months data) string given and 
          * returns an int[12] array with the data 
          **/
-        public int[] dataLineToArray(string ln)
+        public int[] DataLineToArray(string ln)
         {
             int[] monthValues = new int[12];
             monthIndex = 0;
@@ -167,36 +196,10 @@ namespace JBA_Task
             return monthValues;
         }
 
-        /* Logs into the database a year of data with the x and y ref
-         * 
-         **/
-        public void sendToDB(string ln, SqlConnection conn)
-        {
-            string sql = "INSERT INTO PRECIPITATION values (@xRef, @yRef, @Date, @Value)";
-            SqlCommand cmd = new SqlCommand(sql, conn);
-            int[] values = dataLineToArray(ln);
-            string date = "";
-            for (int i = 1; i <= values.Length; i++)
-            {
-                date = yearIndex + "-" + i + "-1";
-                cmd = new SqlCommand(sql, conn);
-                cmd.Parameters.Add("@xRef", SqlDbType.Int);
-                cmd.Parameters.Add("@yRef", SqlDbType.Int);
-                cmd.Parameters.Add("@Date", SqlDbType.Date);
-                cmd.Parameters.Add("@Value", SqlDbType.Int);
-                cmd.Parameters["@xRef"].Value = xRef;
-                cmd.Parameters["@yRef"].Value = yRef;
-                cmd.Parameters["@Date"].Value = date;
-                cmd.Parameters["@Value"].Value = values[i-1];
-                cmd.ExecuteNonQuery();
-            }
-            
-          
-        }
         /*
          * Creates the database on a SQL server 
          **/
-        public void createDatabase()
+        public void CreateDatabase()
         {
             String str = "CREATE DATABASE jbaDB";
             SqlConnection conn = new SqlConnection(serverConn);
@@ -206,7 +209,7 @@ namespace JBA_Task
                 conn.Open();
                 myCommand.ExecuteNonQuery();
                 Console.WriteLine("DataBase is Created Successfully");
-                myCommand = new SqlCommand(table, conn);
+                myCommand = new SqlCommand(createTableSQL, conn);
 
             }
             catch (System.Exception ex)
@@ -222,12 +225,12 @@ namespace JBA_Task
             }
         }
 
-        public void createTable(SqlConnection conn)
+        public void CreateTable(SqlConnection conn)
         {
             try
             {
                 {
-                    SqlCommand cmd = new SqlCommand(table, conn);
+                    SqlCommand cmd = new SqlCommand(createTableSQL, conn);
                     cmd.ExecuteNonQuery();
                     Console.WriteLine("Table is Created Successfully");
                 }
@@ -238,5 +241,39 @@ namespace JBA_Task
                 MessageBox.Show(ex.ToString(), "MyProgram", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
+
+        /* **Used bulk insert instead as it took too much time to insert all the data with one insert at a time**
+        * Logs into the database a year of data with the x and y ref
+        * 
+       public void SendToDB(string ln, SqlConnection conn)
+       {
+           string sql = "INSERT INTO PRECIPITATION values (@xRef, @yRef, @Date, @Value)";
+           string sqlv2 = "INSERT INTO PRECIPITATION values ({0}, {1}, '{2}', {3});";
+           StringBuilder sb = new StringBuilder();
+           // SqlCommand cmd = new SqlCommand(sql, conn);
+           SqlCommand cmd;
+           string date = "";
+
+           int[] values = DataLineToArray(ln);
+           for (int i = 1; i <= values.Length; i++)
+           {
+               date = yearIndex + "-" + i + "-1";
+              // sb.AppendFormat(sqlv2, xRef, yRef, date, values[i - 1]);
+               cmd = new SqlCommand(sql, conn);
+               cmd.Parameters.Add("@xRef", SqlDbType.Int);
+               cmd.Parameters.Add("@yRef", SqlDbType.Int);
+               cmd.Parameters.Add("@Date", SqlDbType.Date);
+               cmd.Parameters.Add("@Value", SqlDbType.Int);
+               cmd.Parameters["@xRef"].Value = xRef;
+               cmd.Parameters["@yRef"].Value = yRef;
+               cmd.Parameters["@Date"].Value = date;
+               cmd.Parameters["@Value"].Value = values[i-1];              
+               cmd.ExecuteNonQuery();
+           }
+          // cmd = new SqlCommand(sb.ToString(), conn);
+          // cmd.ExecuteNonQuery();
+
+       }
+       */
     }
 }
